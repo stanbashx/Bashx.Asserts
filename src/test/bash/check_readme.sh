a89800eb77355ef8436ea0d41ad34db2c9b78403
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-06-17 14:39:34 +0300

    checks

--- src/test/bash/check_readme.sh
+++ src/test/bash/check_readme.sh
@@ -18,6 +18,8 @@ elif [[ ! -s "${ISSUER}" ]]; then
  echo "File \"${ISSUER}\" is empty!"; exit 1
 fi
 
+EXPECTED_NAME="# ${REP_NAME}"
+
 EXPECTED_RELEASE="
 \`${VERSION}\`
 | [GitHub](https://github.com/${REP_OWNER}/${REP_NAME}/releases/tag/${VERSION})
@@ -35,6 +37,7 @@ $ TMP_PATH=\"\$(mktemp)\"; \\
 
 ACTUAL_TEXT="$(< "${ISSUER}")"
 EXPECTED_TEXTS=(
+ "${EXPECTED_NAME}"
  "${EXPECTED_RELEASE}"
  "${EXPECTED_BUILD_AND_INSTALL}"
  "${EXPECTED_DOWNLOAD_AND_INSTALL}"
