acb202dff3fe9399d6ca744f483b378c8a855867
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-06-26 13:38:45 +0300

    tests

--- src/main/bash/fail.sh
+++ src/main/bash/fail.sh
@@ -5,6 +5,9 @@ if [[ $# -ne 1 ]]; then
 
 ASSERTS_MESSAGE="$1"
 
+if [[ -z "${ASSERTS_MESSAGE}" ]]; then
+ echo 'No message!' >&2; exit 1; fi
+
 echo "${ASSERTS_MESSAGE}" >&2
 
 exit 1
---
+++ src/test/bash/fail_test.sh
@@ -0,0 +1,77 @@
+#!/usr/local/bin/bash
+
+SCRIPT='src/main/bash/fail.sh'
+
+echo "Running test for \"${SCRIPT}\"..."
+
+if [[ -L "${SCRIPT}" ]]; then
+ echo "\"${SCRIPT}\" is a symlink!" >&2; exit 1
+elif [[ ! -e "${SCRIPT}" ]]; then
+ echo "\"${SCRIPT}\" does not exist!" >&2; exit 1
+elif [[ ! -f "${SCRIPT}" ]]; then
+ echo "\"${SCRIPT}\" is not a file!" >&2; exit 1
+elif [[ ! -s "${SCRIPT}" ]]; then
+ echo "\"${SCRIPT}\" is empty!" >&2; exit 1
+elif [[ ! -x "${SCRIPT}" ]]; then
+ echo "\"${SCRIPT}\" is not executable!" >&2; exit 1
+elif ! /usr/local/bin/bash -n "${SCRIPT}"; then
+ echo "\"${SCRIPT}\" has invalid syntax!" >&2; exit 1
+fi
+
+STDOUT="$(mktemp)"
+STDERR="$(mktemp)"
+
+#
+
+:> "${STDOUT}"
+:> "${STDERR}"
+"${SCRIPT}" > "${STDOUT}" 2> "${STDERR}"; CODE=$?
+if [[ "${CODE}" != '1' ]]; then
+ echo "Code(${CODE}) error!" >&2; exit 1; fi
+if [[ -n "$(<"${STDOUT}")" ]]; then
+ echo "Script \"${SCRIPT}\" has stdout!" >&2; exit 1; fi
+ACTUAL_VALUE="$(<"${STDERR}")"
+if [[ "${ACTUAL_VALUE}" != 'Wrong arguments!' ]]; then
+ echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1; fi
+
+:> "${STDOUT}"
+:> "${STDERR}"
+"${SCRIPT}" '' '' > "${STDOUT}" 2> "${STDERR}"; CODE=$?
+if [[ "${CODE}" != '1' ]]; then
+ echo "Code(${CODE}) error!" >&2; exit 1; fi
+if [[ -n "$(<"${STDOUT}")" ]]; then
+ echo "Script \"${SCRIPT}\" has stdout!" >&2; exit 1; fi
+ACTUAL_VALUE="$(<"${STDERR}")"
+if [[ "${ACTUAL_VALUE}" != 'Wrong arguments!' ]]; then
+ echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1; fi
+
+#
+
+:> "${STDOUT}"
+:> "${STDERR}"
+ASSERTS_MESSAGE=''
+"${SCRIPT}" "${ASSERTS_MESSAGE}" > "${STDOUT}" 2> "${STDERR}"; CODE=$?
+if [[ "${CODE}" != '1' ]]; then
+ echo "Code(${CODE}) error!" >&2; exit 1; fi
+if [[ -n "$(<"${STDOUT}")" ]]; then
+ echo "Script \"${SCRIPT}\" has stdout!" >&2; exit 1; fi
+ACTUAL_VALUE="$(<"${STDERR}")"
+if [[ "${ACTUAL_VALUE}" != 'No message!' ]]; then
+ echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1; fi
+
+:> "${STDOUT}"
+:> "${STDERR}"
+ASSERTS_MESSAGE='foo'
+"${SCRIPT}" "${ASSERTS_MESSAGE}" > "${STDOUT}" 2> "${STDERR}"; CODE=$?
+if [[ "${CODE}" != '1' ]]; then
+ echo "Code(${CODE}) error!" >&2; exit 1; fi
+if [[ -n "$(<"${STDOUT}")" ]]; then
+ echo "Script \"${SCRIPT}\" has stdout!" >&2; exit 1; fi
+ACTUAL_VALUE="$(<"${STDERR}")"
+if [[ "${ACTUAL_VALUE}" != "${ASSERTS_MESSAGE}" ]]; then
+ echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1; fi
+
+#
+
+rm "${STDOUT}"
+rm "${STDERR}"
