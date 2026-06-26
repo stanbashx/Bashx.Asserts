f215804ccbd2f20f7bc0898da0c09ab8efa5ac63
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-06-26 14:21:09 +0300

    wip

--- src/main/bash/ints/eq.sh
+++ src/main/bash/ints/eq.sh
@@ -13,7 +13,7 @@ ASSERTS_ACTUAL="$2"
 if [[ ! "${ASSERTS_ACTUAL}" =~ ^(0|-?[1-9][0-9]*)$ ]]; then
  echo "Actual(${#ASSERTS_ACTUAL}): \"${ASSERTS_ACTUAL}\" is not a number!" >&2; exit 1
 elif ((ASSERTS_ACTUAL < -2147483648 || ASSERTS_ACTUAL > 2147483647)); then
- echo "Actual: ${ASSERTS_ACTUAL} is not an int64!" >&2; exit 1
+ echo "Actual: ${ASSERTS_ACTUAL} is not an int32!" >&2; exit 1
 fi
 
 ASSERTS_EXPECTED="$3"
@@ -21,7 +21,7 @@ ASSERTS_EXPECTED="$3"
 if [[ ! "${ASSERTS_EXPECTED}" =~ ^(0|-?[1-9][0-9]*)$ ]]; then
  echo "Expected(${#ASSERTS_EXPECTED}): \"${ASSERTS_EXPECTED}\" is not a number!" >&2; exit 1
 elif ((ASSERTS_EXPECTED < -2147483648 || ASSERTS_EXPECTED > 2147483647)); then
- echo "Expected: ${ASSERTS_EXPECTED} is not an int64!" >&2; exit 1
+ echo "Expected: ${ASSERTS_EXPECTED} is not an int32!" >&2; exit 1
 fi
 
 if [[ "${ASSERTS_ACTUAL}" -ne "${ASSERTS_EXPECTED}" ]]; then
